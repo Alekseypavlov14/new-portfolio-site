237ee2d3aa9c73591ad77886ddb166d71d79ae4c
Alekseypavlov14 <aleshapavlov9@gmail.com>
2023-03-14 11:12:41 +0200

    added adaptive (mobile styles) to Badge component

--- src/shared/components/Badge/Badge.tsx
+++ src/shared/components/Badge/Badge.tsx
@@ -1,14 +1,18 @@
 import { FC, ReactNode } from 'react'
 import { H6 } from '@shared/components/Text'
 import styles from './Badge.module.css'
+import cn from 'clsx'
 
 interface BadgeProps {
   children: ReactNode
+  className?: string
 }
 
-export const Badge: FC<BadgeProps> = ({ children }) => {
+export const Badge: FC<BadgeProps> = ({ children, className }) => {
+  const classNames = cn(styles.Badge, className)
+
   return (
-    <H6 className={styles.Badge}>
+    <H6 className={classNames}>
       {children}
     </H6>
   )
