6640ee9859e0b50637233c612cdc54a018c88a69
Alekseypavlov14 <aleshapavlov9@gmail.com>
2023-03-14 22:21:57 +0200

    added providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
+import { QueryClientProvider } from 'react-query'
 import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react'
+import { Provider } from 'react-redux'
+import { client } from '@app/query'
+import { store } from '@app/store'
 import { App } from './App'
 import './index.css'
 
@@ -8,8 +12,12 @@ const root = createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <QueryClientProvider client={client}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </Provider>
   </StrictMode>
 )
