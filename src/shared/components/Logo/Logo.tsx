30439a71f3557fb5c334b27bfebf2946fe15d2b6
Alekseypavlov14 <aleshapavlov9@gmail.com>
2023-03-18 11:11:15 +0200

    changed Logo

--- src/shared/components/Logo/Logo.tsx
+++ src/shared/components/Logo/Logo.tsx
@@ -1,12 +1,19 @@
 import { FC } from 'react'
+import { useNavigation } from '@app/navigation'
 import styles from './Logo.module.css'
+import logo from './logo.svg'
 
 interface LogoProps {}
 
 export const Logo: FC<LogoProps> = () => {
+  const { navigateHomePage } = useNavigation()
+
   return (
-    <div className={styles.Logo}>
-      Logo
-    </div>
+    <img 
+      onClick={navigateHomePage}
+      className={styles.Logo}
+      src={logo} 
+      alt='ALEX' 
+    />
   )
 }
