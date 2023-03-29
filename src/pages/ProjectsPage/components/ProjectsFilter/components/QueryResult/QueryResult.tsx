8e5b06e437c5d5868ef9ed81a3c63740215e0965
Alekseypavlov14 <aleshapavlov9@gmail.com>
2023-03-29 21:13:45 +0300

    fixed bugs ad changed Appear Animation animation-duration from 1s to 0.7s

--- src/pages/ProjectsPage/components/ProjectsFilter/components/QueryResult/QueryResult.tsx
+++ src/pages/ProjectsPage/components/ProjectsFilter/components/QueryResult/QueryResult.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react'
 import { resultSelector, useSearchStore } from '../../store'
 import { Appear, Directions } from '@shared/components/Animations'
 import { ProjectCard } from '@widgets/ProjectCard'
+import { Decoration } from '@shared/components/Decoration'
 import styles from './QueryResult.module.css'
 
 interface QueryResultProps {}
@@ -10,16 +11,23 @@ export const QueryResult: FC<QueryResultProps> = () => {
   const projects = useSearchStore(resultSelector)
 
   return (
-    <Appear 
-      className={styles.QueryResult}
-      from={Directions.Bottom} 
-    >
-      {projects.map(project => (
-        <ProjectCard 
-          project={project} 
-          key={project.id} 
-        />
-      ))}
-    </Appear>
+    <div className={styles.QueryResultWrapper}>
+      <Appear 
+        className={styles.QueryResult}
+        from={Directions.Bottom} 
+      >
+        {projects.map(project => (
+          <ProjectCard 
+            project={project} 
+            key={project.id} 
+          />
+        ))}
+      </Appear>
+
+      <Decoration className={styles.Decoration1} />
+      <Decoration className={styles.Decoration2} />
+      <Decoration className={styles.Decoration3} />
+      <Decoration className={styles.Decoration4} />
+    </div>
   )
 }
